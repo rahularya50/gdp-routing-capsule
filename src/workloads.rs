af4ea2a9a15a1658b32d0db7e260d64d5b59e0b3
Ja (Thanakul) Wattanawong <ja.wattanawong@gmail.com>
2021-11-30 05:18:22 -0800

    Add testing config code

--- src/workloads.rs
+++ src/workloads.rs
@@ -3,10 +3,14 @@ use crate::gdp::Gdp;
 use crate::gdp::GdpAction;
 use crate::kvs::{GdpName, Store};
 use crate::schedule::Schedule;
+use rand::Rng;
 use crate::startup_route_lookup;
+use std::fs;
 
 use crate::statistics::make_print_stats;
 use crate::Route;
+use serde::Deserialize;
+
 use anyhow::{anyhow, Result};
 use capsule::batch::{self, Batch, Pipeline};
 use capsule::config::RuntimeConfig;
@@ -24,7 +28,20 @@ use std::net::Ipv4Addr;
 use std::time::Duration;
 use tokio_timer::delay_for;
 
-const MSG: &[u8] = &[b'A'; 772];
+const MSG: &[u8] = &[b'A'; 10000];
+
+#[derive(Clone, Copy, Deserialize)]
+pub struct TestConfig {
+    pub payload_size: usize,
+    pub random_dest_chance: f32,
+}
+
+pub fn load_test_config() -> Result<TestConfig> {
+    let content = fs::read_to_string("test.toml")?;
+    let test_config: TestConfig = toml::from_str(&content)?;
+
+    Ok(test_config)
+}
 
 fn prep_packet(
     reply: Mbuf,
@@ -32,6 +49,8 @@ fn prep_packet(
     src_ip: Ipv4Addr,
     dst_mac: MacAddr,
     dst_ip: Ipv4Addr,
+    payload_size: usize,
+    random_dest_chance: f32,
 ) -> Result<Gdp<Ipv4>> {
     let mut reply = reply.push::<Ethernet>()?;
     reply.set_src(src_mac);
@@ -49,13 +68,20 @@ fn prep_packet(
 
     let mut reply = reply.push::<Gdp<Ipv4>>()?;
     reply.set_action(GdpAction::Forward);
-    reply.set_dst(3);
+
+    let mut rng = rand::thread_rng();
+    let rval: f32 = rng.gen();
+    if rval < random_dest_chance {
+        reply.set_dst(rng.gen());
+    } else {
+        reply.set_dst(3);
+    }
 
     let message = MSG;
 
     let offset = reply.payload_offset();
-    reply.mbuf_mut().extend(offset, message.len())?;
-    reply.mbuf_mut().write_data_slice(offset, &message)?;
+    reply.mbuf_mut().extend(offset, payload_size)?;
+    reply.mbuf_mut().write_data_slice(offset, &message[..payload_size])?;
 
     reply.reconcile_all();
 
@@ -69,10 +95,12 @@ fn send_initial_packets(
     src_ip: Ipv4Addr,
     switch_route: Route,
     num_packets: usize,
+    payload_size: usize,
+    random_dest_chance: f32
 ) -> () {
     let src_mac = q.mac_addr();
     batch::poll_fn(|| Mbuf::alloc_bulk(num_packets).unwrap())
-        .map(move |packet| prep_packet(packet, src_mac, src_ip, switch_route.mac, switch_route.ip))
+        .map(move |packet| prep_packet(packet, src_mac, src_ip, switch_route.mac, switch_route.ip, payload_size, random_dest_chance))
         .for_each(move |_packet| {
             //println!(
             //    "Sending out one-shot packet from NIC {:?}: {:?}",
@@ -96,7 +124,7 @@ fn send_initial_packet(
     src_ip: Ipv4Addr,
     switch_route: Route,
 ) {
-    send_initial_packets(q, nic_name, store, src_ip, switch_route, 1);
+    send_initial_packets(q, nic_name, store, src_ip, switch_route, 1, 800,0.0);
 }
 
 pub fn dev_schedule(q: PortQueue, name: &str, store: Store) -> impl Pipeline + '_ {
@@ -123,11 +151,15 @@ fn client_schedule(q: PortQueue, name: &str, store: Store, src_ip: Ipv4Addr) -> 
     })
 }
 
-fn spammy(q: PortQueue, name: &str, store: Store, src_ip: Ipv4Addr) -> impl Pipeline + '_ {
+fn flood_single(q: PortQueue, name: &str, store: Store, src_ip: Ipv4Addr) -> impl Pipeline + '_ {
     let switch_route = startup_route_lookup(2).unwrap();
+    let test_conf = load_test_config().unwrap_or_else(|_| TestConfig{payload_size: 800, random_dest_chance: 0.0});
+    let payload_size = test_conf.payload_size;
+    let random_dest_chance = test_conf.random_dest_chance;
+    println!("Running test {} with payload size {}, random dest chance {}", "flood_single", payload_size, random_dest_chance);
     Schedule::new(name, async move {
-        loop {
-            send_initial_packets(q.clone(), name, store, src_ip, switch_route, 36);
+        for _i in 0..100000 {
+            send_initial_packets(q.clone(), name, store, src_ip, switch_route, 36, payload_size, random_dest_chance);
             delay_for(Duration::from_micros(1));
         }
     })
@@ -138,7 +170,7 @@ pub fn start_client_server(config: RuntimeConfig, gdp_name: GdpName) -> Result<(
     let (print_stats, history_map) = make_print_stats();
     let src_route = startup_route_lookup(gdp_name).ok_or(anyhow!("Invalid client GDPName!"))?;
     Runtime::build(config)?
-        .add_pipeline_to_port("eth1", move |q| spammy(q, "client", store, src_route.ip))?
+        .add_pipeline_to_port("eth1", move |q| flood_single(q, "client", store, src_route.ip))?
         .add_periodic_task_to_core(
             0,
             print_stats,
